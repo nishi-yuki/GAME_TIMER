382e3be56722bf64150f7e4dabfcae2bbf386bcb
nishi-yuki <nishiyk256@gmail.com>
2018-09-23 23:59:44 +0900

    add test file DataManagerTest.kt

---
+++ app/src/test/java/jp/gr/javaConf/nishi1998ez/gameTimer/DataManagerTest.kt
@@ -0,0 +1,28 @@
+package jp.gr.javaConf.nishi1998ez.gameTimer
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class DataManagerTest {
+
+    @Test
+    fun getONE_DAY_MILLIS() {
+    }
+
+    @Test
+    fun getPoint() {
+    }
+
+    @Test
+    fun setPoint() {
+    }
+
+    @Test
+    fun getMillisTimeSinceLastUse() {
+    }
+
+    @Test
+    fun save() {
+    }
+}
